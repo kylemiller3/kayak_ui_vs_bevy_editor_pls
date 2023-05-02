c361f37bf71abdd17608eb167b2723e9a409cf21
Kyle Miller <k@crashoverride2.local>
2023-05-02 03:22:17 -0400

    No font rendered

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,47 @@
 use bevy::prelude::*;
 use bevy_editor_pls::EditorPlugin;
-use kayak_ui::{widgets::KayakWidgets, prelude::KayakContextPlugin};
+use kayak_ui::{
+    prelude::{widgets::*, *},
+    CameraUIKayak,
+};
+
+fn startup(
+    mut commands: Commands,
+    mut font_mapping: ResMut<FontMapping>,
+    asset_server: Res<AssetServer>,
+) {
+    let camera_entity = commands
+        .spawn(Camera2dBundle::default())
+        .insert(CameraUIKayak)
+        .id();
+
+    font_mapping.set_default(asset_server.load("roboto.kayak_font"));
+    // font_mapping.force_subpixel(&asset_server.load("roboto.kayak_font"));
+
+    let mut widget_context = KayakRootContext::new(camera_entity);
+    widget_context.add_plugin(KayakWidgetsContextPlugin);
+    let parent_id = None;
+    rsx! {
+        <KayakAppBundle>
+            <TextWidgetBundle
+                text={TextProps {
+                    content: "Hello World".into(),
+                    size: 20.0,
+                    ..Default::default()
+                }}
+            />
+        </KayakAppBundle>
+    };
+
+    commands.spawn((widget_context, EventDispatcher::default()));
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // .add_plugin(EditorPlugin::default())
         .add_plugin(KayakContextPlugin)
         .add_plugin(KayakWidgets)
-        .add_plugin(EditorPlugin::default())
-        .run();
+        .add_startup_system(startup)
+        .run()
 }
